7938d8e285d2d91cca4dfb586454818fdb2babd0
xuewang <xuewang@thoughtworks.com>
2020-07-02 21:25:28 +0800

    implement es6 practices

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,9 @@
-const parseData
+const parseData = (input) =>
+  input.data.map((row) =>
+    row.reduce(
+      (result, curr, index) =>
+        (result = { ...result, [input.column[index].name]: curr }),
+      {}
+    )
+  );
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,9 @@
-const inject
+const inject = (items, sections) => {
+  let result = [...items];
+  const orderedSections = sections.sort(
+    (objA, objB) => objB.index - objA.index
+  );
+  orderedSections.forEach((s) => result.splice(s.index, 0, s.content));
+  return result;
+};
 export { inject };
